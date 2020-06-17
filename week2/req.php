79c21e11ff8a166ba7af0236579bb678e34d923b
Gowri Rajesh <gowrirajesh6@gmail.com>
2020-06-17 09:17:20 +0530

    Update req.php

--- week2/req.php
+++ week2/req.php
@@ -1,5 +1,6 @@
-<?php require "mean.php"; ?>
+<?php require "mean.php"; ?> 
+<!--non existent file format is mean.php so as to quote the differnce between include and php-->
 
     <h1>Welcome to Our Website!</h1>
     <p>Here you will find lots of useful resources.</p>
-<?php require "what.php"; ?>
+<?php require "footer.php"; ?>
