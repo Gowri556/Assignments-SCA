d24ba669f93ff9fa84506f009ba096ad0440ba53
Gowri Rajesh <gowrirajesh6@gmail.com>
2020-06-17 08:48:24 +0530

    Main file

---
+++ week2/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: black;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+
+<footer>
+  <p>Author: Hege Refsnes<br>
+  <a href="mailto:hege@example.com">hege@example.com</a></p>
+</footer>
+
+</body>
+</html>
---
+++ week2/test11.php
@@ -0,0 +1,6 @@
+
+<?php include "header.php"; ?>
+<?php include "menu.php"; ?>
+    <h1>Welcome to Our Website!</h1>
+    <p>Here you will find lots of useful resources.</p>
+<?php include "footer.php"; ?>
